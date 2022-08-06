e6e55561b9ea61557990de3451325a6c17057822
MsNahid <m.nahidzaman@gmail.com>
2022-08-06 23:12:48 +0600

    added currency conversion service

--- currency-exchange-service/src/main/java/com/nahidsohel/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/nahidsohel/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -6,20 +6,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-
 @RestController
 @RequiredArgsConstructor
 public class CurrencyExchangeController {
 
     private final Environment environment;
 
-    @GetMapping("/currency-exchange/{from}/USD/{to}/INR")
+    private final CurrencyExchangeRepository currencyExchangeRepository;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveCurrencyExchange(@PathVariable String from, @PathVariable String to){
 
         String port = environment.getProperty("local.server.port");
 
-        CurrencyExchange currencyExchange = new CurrencyExchange(100L, from, to, BigDecimal.valueOf(50), port);
+//        CurrencyExchange currencyExchange = new CurrencyExchange(100L, from, to, BigDecimal.valueOf(50), port);
+
+        CurrencyExchange currencyExchange  = currencyExchangeRepository.findByFromAndTo(from, to);
+
+        if(currencyExchange == null){
+            throw new RuntimeException("Unable to find currency exchange " + from + " to " + to + " .");
+        }
 
         return currencyExchange;
 
